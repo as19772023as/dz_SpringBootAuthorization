b4a7f4e0d5fcb519bc528b905b0c17e97eeac8f7
Andrey Strebkov <as19772023as@gmail.com>
2024-02-28 16:48:22 +0300

    add testcontainer

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM openjdk:17-jdk-slim
 
 EXPOSE 8080
 
-COPY target/dz_SpringBootAuthorization-0.0.1-SNAPSHOT.jar app.jar
+COPY target/dz_SpringBootAuthorization-0.0.1-SNAPSHOT.jar myapp.jar
 
-CMD ["java", "-jar", "ap.jar"]
+CMD ["java", "-jar", "myapp.jar"]
--- src/test/java/ru/strebkov/dz_SpringBootAuthorization/DzSpringBootAuthorizationApplicationTests.java
+++ src/test/java/ru/strebkov/dz_SpringBootAuthorization/DzSpringBootAuthorizationApplicationTests.java
@@ -1,13 +1,39 @@
 package ru.strebkov.dz_SpringBootAuthorization;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@DisplayName("Проверка авторизации")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DzSpringBootAuthorizationApplicationTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private static final GenericContainer<?> devContainer = new GenericContainer<>("authorizationapp")
+            .withExposedPorts(8080);
+    private static final String ENDPOINT = "/authorize?user=Ana&password=123";
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    public static void setUp() {
+        devContainer.start();
+    }
 
+    @Test
+    @DisplayName("Тест на вход")
+    void contextLoads() {
+        Integer devPort = devContainer.getMappedPort(8080);
+        final String expected = "[\"READ\",\"WRITE\",\"DELETE\"]";
+
+        ResponseEntity<String> actual = restTemplate.getForEntity("http://localhost:" + devPort + ENDPOINT, String.class);
+
+        System.out.println("actual =   " + actual.getBody());
+        Assertions.assertEquals(expected, actual.getBody());
+    }
+    
 }
